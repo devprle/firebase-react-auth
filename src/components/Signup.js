0ccd2abcee95b3387813084368ea4f2be8b4a57f
devprle <badprle@gmail.com>
2022-06-28 11:59:28 +0200

    Signup page updated

---
+++ src/components/Signup.js
@@ -0,0 +1,35 @@
+import React, {useRef} from 'react'
+import {Form, Button, Card} from 'react-bootstrap'
+
+export default function Signup() {
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+
+    return (<>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4">Sign Up</h2>
+                <Form>
+                    <Form.Group style={{marginBottom: "10px"}} id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" ref={emailRef} required/>
+                    </Form.Group>
+                    <Form.Group style={{marginBottom: "10px"}} id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} required/>
+                    </Form.Group>
+                    <Form.Group style={{marginBottom: "10px"}} id="password-confirm">
+                        <Form.Label>Password Confirmation</Form.Label>
+                        <Form.Control type="password" ref={passwordConfirmRef} required/>
+                    </Form.Group>
+                    <Button className="w-100" type="submit">Sign In</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+            Already have an account?Log In
+        </div>
+    </>)
+}
